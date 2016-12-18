922867178b2ed60a82675d27bcaf0ba6ed06d725
joellegg <joellegg@bellsouth.net>
2016-12-18 14:57:26 -0600

    reformatted commented classes and bots and added to damageBonus

---
+++ scripts/enemies.js
@@ -0,0 +1,4 @@
+// 3 classes: Star Wars, Transformers, & Misc
+// Star Wars: 3CPO, R2D2, BB-8 vs. B1 Battle Droids, imperial probe droid, AT-AT
+// Transformers: Optimus, Bumble Bee, Sentinel Prime & Ironhide vs... Megatron, starscream, schockwave, & galvatron
+// Misc: WALL-E, Terminator, Dot (spaceballs), Bicentennial Man vs. Auto (WALL-E), Vanessa Kensington (austin powers), Gort (Day the earth stood still), RoboCop
