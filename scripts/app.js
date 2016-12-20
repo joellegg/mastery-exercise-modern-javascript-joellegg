b53af51709be706e4f2858fcdf384f0c1a97507d
joellegg <joellegg@bellsouth.net>
2016-12-20 09:59:02 -0600

    changed a to buttons and switched to simplified branch

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,9 @@
+// Show the correct html sections
 $(document).ready(function() {
-    /*
-        Show the initial view that accepts player name
-     */
+    /* Show the initial view that accepts player name */
     $("#player-setup").show();
 
-    /*
-        When any button with card__link class is clicked,
-        move on to the next view.
-     */
+    // When any button with card__link class is clicked, move on to the next view.
     $(".card__link").click(function(e) {
         var nextCard = $(this).attr("next");
         var moveAlong = false;
@@ -15,12 +11,9 @@ $(document).ready(function() {
         switch (nextCard) {
             case "card--class":
                 moveAlong = ($("#player-name").val() !== "") && ($("#enemy-name").val() !== "");
-                console.log('switching to robot card', moveAlong)
                 break;
             case "card--robot":
-                // change to only be true if a button is selected
                 moveAlong = ($(".classButton").hasClass("borderClick"));
-                console.log('switching to card--path', moveAlong)
                 break;
             case "card--battleground":
                 moveAlong = ($(".classButton").hasClass("borderClick") === true);
@@ -35,7 +28,7 @@ $(document).ready(function() {
         //If no name, class or weapon selected and user tries to advance to next page, an alert will pop up.
 
         if (moveAlong === false && ($("#player-name").val() == "")) {
-            alert('Hey listen! I\'d call you by your name, but I don\'t know it!')
+            alert('Hey listen! I\'d call you by your name, but I don\'t know it!');
         } else if
             (moveAlong === false && ($(".classButton").hasClass("borderClick") === false)) {
             alert('I personally recommend you choose to be surprised.')
@@ -43,36 +36,8 @@ $(document).ready(function() {
 
     });
 
-    // If the enter key is pressed on the input field, it will advance to the next page
+    // When the back button clicked, move back a view
 
-    $("#player-name").keypress(function(e) {
-        var nextCard = $(this).attr("next");
-        var moveAlong = false;
-
-
-        if (($("#player-name").val() !== "" && e.key === 'Enter')) {
-            moveAlong = true;
-            nextCard = 'card--class'
-        }
-
-        if (moveAlong) {
-            $(".card").hide();
-            $("." + nextCard).show();
-        }
-
-        // If no name is inserted and the enter is pressed an alert will pop up.
-
-        if (moveAlong === false && (e.key === "Enter")) {
-            alert('Hey listen! I\'d call you by your name, but I don\'t know it!')
-        };
-
-    });
-
-
-
-    /*
-        When the back button clicked, move back a view
-     */
     $(".card__back").click(function(e) {
         var previousCard = $(this).attr("previous");
         $(".card").hide();
